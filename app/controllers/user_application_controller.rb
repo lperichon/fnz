f773a12e508317851a2bd9b3947e68564084328d
Luis Perichon <luisperichon@gmail.com>
2012-10-16 17:24:12 -0300

    Allow user to change its timezone

--- app/controllers/user_application_controller.rb
+++ app/controllers/user_application_controller.rb
@@ -1,8 +1,13 @@
 class UserApplicationController < ApplicationController
   before_filter :authenticate_user!
   before_filter :set_current_user
+  before_filter :set_time_zone
 
   def set_current_user
     User.current_user = current_user if current_user
+  end
+
+  def set_time_zone
+    Time.zone = current_user.time_zone if current_user
   end
 end
