85cd0fe95b534558e8dbf45d39da962c86d24638
Luis Perichon <luisperichon@gmail.com>
2012-10-16 23:47:56 -0300

    fix bug when updating user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
   include TzMagic::BeforeFilter
 
-  skip_before_filter :ensure_timezone, :only => :edit
+  skip_before_filter :ensure_timezone, :only => [:edit, :update, :create]
 end
