fc2cc3f4f03b20680abcfda3e497ef9a08c5c9aa
Luis Perichon <luisperichon@gmail.com>
2013-01-30 13:35:02 -0300

    page padma_contacts

--- app/models/padma_contacts_synchronizer.rb
+++ app/models/padma_contacts_synchronizer.rb
@@ -6,17 +6,24 @@ class PadmaContactsSynchronizer
   end
 
   def sync
-    PadmaContact.search(:where => {:updated_at =>  business.synchronized_at},
-                        :account_name => business.padma_id).each do |padma_contact|
-      contact = Contact.find_or_create_by_padma_id(:padma_id => padma_contact.id,
+    page = 1
+    more_contacts = true
+    while(more_contacts)
+      padma_contacts = PadmaContact.search(:where => {:updated_at =>  business.synchronized_at},
+                        :account_name => business.padma_id,
+                        :per_page => 100,
+                        :page => page).each do |padma_contact|
+        contact = Contact.find_or_create_by_padma_id(:padma_id => padma_contact.id,
                                                    :business_id => business.id,
                                                    :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip)
-      #TODO: update cached values on existing contacts
-      #unless contact.created_at < 10.seconds.ago
-      #  contact.update_attributes
-      #end
+        #TODO: update cached values on existing contacts
+        #unless contact.created_at < 10.seconds.ago
+        #  contact.update_attributes
+        #end
+      end
+      more_contacts = !padma_contacts.empty?
+      page = page + 1
     end
-
     business.update_attribute(:synchronized_at, Date.today)
   end
 end
