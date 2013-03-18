6be1c90595030219ff32334bbca482bc0536c87a
Luis Perichon <luisperichon@gmail.com>
2013-03-18 14:29:14 -0300

    fix bug on render

--- app/inputs/date_time_picker_input.rb
+++ app/inputs/date_time_picker_input.rb
@@ -2,7 +2,7 @@ class DateTimePickerInput < SimpleForm::Inputs::Base
   def input
     out = '' # the output string we're going to build
     value = object.send(attribute_name)
-    out << template.text_field_tag("datepicker_" + attribute_name.to_s, value ? value.to_s(:datepicker) : "", :class => "datepicker_input inline")
+    out << template.text_field_tag("datepicker_" + attribute_name.to_s, value ? value.toz_s(:datepicker) : "", :class => "datepicker_input inline")
     out << template.text_field_tag("timepicker_" + attribute_name.to_s, value ? value.to_s(:timepicker) : "", :class => "timepicker_input inline")
     (out << @builder.hidden_field(attribute_name, input_html_options)).html_safe
   end
