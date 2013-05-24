69eadc288163eb0776f477b8df01c6fee54fe896
Luis Perichon <luisperichon@gmail.com>
2013-05-24 14:48:52 -0300

    fix unicorn for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,3 +9,9 @@ preload_app true
 #  @resque_pid ||= spawn("bundle exec rake " + \
 #  "resque:work QUEUES=uploader,mailer")
 #end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection 
+  end
+end
