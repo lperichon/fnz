7c2c929a2896866243b3081292773e4a3b26e29c
Luis Perichon <luisperichon@gmail.com>
2013-05-24 14:15:22 -0300

    dont use a worker for resque (for now)

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,9 +3,9 @@ timeout 30
 
 preload_app true
 
-@resque_pid = nil
+#@resque_pid = nil
 
-before_fork do |server, worker|
-  @resque_pid ||= spawn("bundle exec rake " + \
-  "resque:work QUEUES=uploader,mailer")
-end
+#before_fork do |server, worker|
+#  @resque_pid ||= spawn("bundle exec rake " + \
+#  "resque:work QUEUES=uploader,mailer")
+#end
