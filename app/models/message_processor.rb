e6139cee7f5f78ffb3e305995a22a3e89ecc4207
Luis Perichon <luisperichon@gmail.com>
2014-01-18 22:05:27 -0300

    remove unnecesary logs

--- app/models/message_processor.rb
+++ app/models/message_processor.rb
@@ -1,18 +1,16 @@
 class MessageProcessor
   def self.catch_message(key_name, data)
-    Rails.logger.debug "catch"
     return unless key_name == 'subscription_change'
-    Rails.logger.debug "message 'subscription_change'"
+    
     business = Business.find_by_padma_id(data[:account_name])
     return if business.nil?
-    Rails.logger.debug "for Business #{business.name}"
+    
     if data[:type] == 'Enrollment'
-      Rails.logger.debug 'Enrollment'
       #Create local contact
       padma_contact = PadmaContact.find(data[:contact_id],
                                         :select => [:first_name, :last_name, :status, :global_teacher_username],
                                         :account_name => business.padma_id)
-      Rails.logger.debug "for padma_contact [##{data[:contact_id]}] #{padma_contact.first_name} #{padma_contact.last_name}"
+      
       contact = Contact.find_or_create_by_padma_id(:padma_id => padma_contact.id,
                                                      :business_id => business.id,
                                                      :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
@@ -24,7 +22,6 @@ class MessageProcessor
             :padma_status => padma_contact.status,
             :padma_teacher => padma_contact.global_teacher_username)
       end
-      Rails.logger.debug "local contact ##{contact.id} #{contact.created_at >= 1.minute.ago ? "created" : "found"}"
       # TODO: Create Membership using data from message
     elsif data[:type] == 'DropOut'
       #TODO: Close membership
