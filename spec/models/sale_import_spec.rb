2749093d20c311691de19a01545db7b6933099b3
Luis Perichon <luisperichon@gmail.com>
2014-01-20 09:46:40 -0300

    [#63047520]add back file (?)

---
+++ spec/models/sale_import_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe SaleImport do
+  
+  before(:each) do
+    @business = FactoryGirl.create(:business)
+    @attr = {
+      :upload => Rack::Test::UploadedFile.new(Rails.root.join('spec','fixtures', 'empty_ventas.csv'), 'text/csv'),
+      :business_id => @business.id,
+      :status => :ready
+    }
+
+    User.current_user = @business.owner
+  end
+
+  it "should process a csv file" do
+    import = SaleImport.create(@attr)
+    import.upload.stub(:path).and_return("#{Rails.root}/spec/fixtures/belgrano_ventas.csv")
+    expect {
+      import.process
+    }.to change(Sale, :count).by(198)
+    import.status.should == :finished
+  end
+
+
+end
