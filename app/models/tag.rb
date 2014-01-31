6cd87d2556ec54133148d01b36a4ea35096b8d68
Luis Perichon <luisperichon@gmail.com>
2014-01-31 19:50:19 -0300

    revert order

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,7 +6,7 @@ class Tag < ActiveRecord::Base
   validates :name, :presence => true
   validates :business, :presence => true
 
-  default_scope order('name DESC')
+  default_scope order('name ASC')
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :business_id
