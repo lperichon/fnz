459765d9ac85a92817e4cd1fc2b40f57140ad247
Luis Perichon <luisperichon@gmail.com>
2014-02-08 16:49:55 -0300

    [#36112169]configure mandrill smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,16 +22,13 @@ Fnz::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
   }
-
-
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
