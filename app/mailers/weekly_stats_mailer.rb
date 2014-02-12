9f9ad6985af00416bcf9017a12565a5142f8e626
Luis Perichon <luisperichon@gmail.com>
2014-02-12 12:44:51 -0300

    [#36112169]weekly stats mailer lists memberships

--- app/mailers/weekly_stats_mailer.rb
+++ app/mailers/weekly_stats_mailer.rb
@@ -1,13 +1,13 @@
 require 'httparty'
 
-class WeeklyStatsMailer < MandrillMailer::TemplateMailer
+class WeeklyStatsMailer < MandrillMailer::TemplateMailer	
   default from: 'fnz@padm.am'
 
   def stats(business)
-  	transaction_stats = TransactionStats.new(:business => business)
+  	ac = ActionController::Base.new()
 
-  	renderer = TransactionStatsRenderer.new(:transaction_stats => transaction_stats)
-	
+  	transaction_stats = TransactionStats.new(:business => business)
+	renderer = TransactionStatsRenderer.new(:transaction_stats => transaction_stats)
 	credits_chart = HTTParty.post('http://fnz-highcharts-api.herokuapp.com/chart_images', body: {input: renderer.credits_chart, width:560})
 	debits_chart =  HTTParty.post('http://fnz-highcharts-api.herokuapp.com/chart_images', body: {input: renderer.debits_chart, width:560})
 	credits_image = Tempfile.new(['credits_chart', '.png'], :encoding => 'ascii-8bit')
@@ -16,13 +16,17 @@ class WeeklyStatsMailer < MandrillMailer::TemplateMailer
 	debits_image.write debits_chart.body
     credits_image.rewind
     debits_image.rewind
+
+    contacts = business.contacts.all_students
+
     mandrill_mail template: 'weekly-stats',
                   subject: "Weekly Stats",
                   to: { email: business.owner.email, name: business.owner.name },
                   vars: {
                     'business_name' => business.name,
                     'debits_content' => '',
-                    'credits_content' => ''
+                    'credits_content' => '',
+                    'installments_content' => ac.render_to_string(:partial => 'weekly_stats_mailer/installments_sidebar', :locals => {:contacts => contacts})
                   },
                   important: true,
                   inline_css: true,
