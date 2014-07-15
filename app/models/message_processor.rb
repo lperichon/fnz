3eb7fd0987609c9a98c34ffc015de016a65f4c81
Luis Perichon <luisperichon@gmail.com>
2014-07-15 18:34:30 -0300

    small fixes here & there@

--- app/models/message_processor.rb
+++ app/models/message_processor.rb
@@ -5,27 +5,29 @@ class MessageProcessor
     business = Business.find_by_padma_id(data[:account_name])
     return if business.nil?
     
+    #Create local contact
+    padma_contact = PadmaContact.find(data[:contact_id],
+                                      :select => [:first_name, :last_name, :status, :global_teacher_username],
+                                      :account_name => business.padma_id)
+    
+    contact = Contact.find_or_create_by_padma_id(:padma_id => padma_contact.id,
+                                                   :business_id => business.id,
+                                                   :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
+                                                   :padma_status => padma_contact.status,
+                                                   :padma_teacher => padma_contact.global_teacher_username)
+
+    unless contact.created_at && contact.created_at > 10.seconds.ago
+      contact.update_attributes(
+          :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
+          :padma_status => padma_contact.status,
+          :padma_teacher => padma_contact.global_teacher_username,
+          :business_id => business.id)
+    end
+
     if data[:type] == 'Enrollment'
-      #Create local contact
-      padma_contact = PadmaContact.find(data[:contact_id],
-                                        :select => [:first_name, :last_name, :status, :global_teacher_username],
-                                        :account_name => business.padma_id)
-      
-      contact = Contact.find_or_create_by_padma_id(:padma_id => padma_contact.id,
-                                                     :business_id => business.id,
-                                                     :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
-                                                     :padma_status => padma_contact.status,
-                                                     :padma_teacher => padma_contact.global_teacher_username)
-      unless contact.created_at && contact.created_at > 10.seconds.ago
-        contact.update_attributes(
-            :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
-            :padma_status => padma_contact.status,
-            :padma_teacher => padma_contact.global_teacher_username,
-            :business_id => business.id)
-      end
       # TODO: Create Membership using data from message
     elsif data[:type] == 'DropOut'
-      #TODO: Close membership
+      # TODO: Close Membership
     end
   end
 end
