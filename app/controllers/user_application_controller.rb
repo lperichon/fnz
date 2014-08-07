78b135aa7ae9dfb12fbac9fc9ceeb6341159e6dc
Luis Perichon <luisperichon@gmail.com>
2014-08-07 10:29:50 -0300

    starting with translations

--- app/controllers/user_application_controller.rb
+++ app/controllers/user_application_controller.rb
@@ -2,6 +2,7 @@ class UserApplicationController < ApplicationController
   before_filter :authenticate_user!
   before_filter :set_current_user
   before_filter :set_time_zone
+  before_filter :set_locale
 
   def set_current_user
     User.current_user = current_user if current_user
@@ -9,5 +10,13 @@ class UserApplicationController < ApplicationController
 
   def set_time_zone
     Time.zone = current_user.time_zone if current_user
+  end
+
+  def set_locale
+  	user_locale = current_user.try :locale
+  	I18n.locale = user_locale if user_locale
+
+    # If locale is sent by params override all
+    I18n.locale = params[:locale] if params[:locale]
   end
 end
---
+++ config/initializers/localeapp.rb
@@ -0,0 +1,5 @@
+require 'localeapp/rails'
+
+Localeapp.configure do |config|
+	config.api_key = 'dNVpqmPqBPL4oMz7I148dxdXlHeloQ2av8XWwaWXxZCzg5rss3'
+end
