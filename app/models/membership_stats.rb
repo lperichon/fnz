f6ca544e52bbd6af011ed6b054ad4751c40d092a
Dwayne Macgowan <dwaynemac@gmail.com>
2014-10-13 02:06:40 -0300

    allow membership_stats to filter memberhsips

--- app/models/membership_stats.rb
+++ app/models/membership_stats.rb
@@ -3,7 +3,7 @@ class MembershipStats
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :business, :month, :year
+  attr_accessor :business, :month, :year, :membership_filter
 
   validates_presence_of :business, :month, :year
 
@@ -18,10 +18,18 @@ class MembershipStats
   end
 
   def enrollments
-    business.memberships.joins(:enrollment).joins(:enrollment => :transactions).where("enrolled_on >= '#{Date.new(year, month, 1).beginning_of_month.beginning_of_day}' AND enrolled_on <= '#{Date.new(year, month, 1).end_of_month.end_of_day}'").select("SUM(CASE WHEN transactions.amount > enrollments.value THEN enrollments.value ELSE transactions.amount END) AS sum")
+    memberships.joins(:enrollment).joins(:enrollment => :transactions).where("enrolled_on >= '#{Date.new(year, month, 1).beginning_of_month.beginning_of_day}' AND enrolled_on <= '#{Date.new(year, month, 1).end_of_month.end_of_day}'").select("SUM(CASE WHEN transactions.amount > enrollments.value THEN enrollments.value ELSE transactions.amount END) AS sum")
   end
 
   def installments
-    business.memberships.joins(:installments).joins(:installments => :transactions).where("due_on >= '#{Date.new(year, month, 1).to_date.beginning_of_month.beginning_of_day}' AND due_on <= '#{Date.new(year, month, 1).end_of_month.end_of_day}'").select("SUM(CASE WHEN transactions.amount > installments.value THEN installments.value ELSE transactions.amount END) AS sum")
+    memberships.joins(:installments).joins(:installments => :transactions).where("due_on >= '#{Date.new(year, month, 1).to_date.beginning_of_month.beginning_of_day}' AND due_on <= '#{Date.new(year, month, 1).end_of_month.end_of_day}'").select("SUM(CASE WHEN transactions.amount > installments.value THEN installments.value ELSE transactions.amount END) AS sum")
+  end
+
+  def memberships
+    @memberships = Membership.where(business_id: business.id)
+    unless @membership_filter.nil?
+      @memberships = @memberships.where(@membership_filter)
+    end
+    @memberships
   end
 end
--- spec/models/membership_stats_spec.rb
+++ spec/models/membership_stats_spec.rb
@@ -10,6 +10,21 @@ describe MembershipStats do
   	FactoryGirl.build(:membership_stats, :business => nil).should_not be_valid
   end
 
+  describe "with membership_filter" do
+    let(:business){FactoryGirl.create(:business)}
+    let(:payment_type){FactoryGirl.create(:payment_type, business: business)}
+    before do
+  		@membership_stats = FactoryGirl.build(:membership_stats, :business => business, :year => Date.today.year, :month => Date.today.month, :membership_filter => { :payment_type_id => payment_type.id})
+  		@membership = FactoryGirl.create(:membership, :business => business, :payment_type_id => nil)
+  		@installment = FactoryGirl.create(:installment, :membership => @membership, :due_on => Date.today)
+  		@transaction = FactoryGirl.create(:transaction, :type => "Credit", :transaction_at => Date.today, :business => business, :creator => business.owner)
+  		@installment.transactions << @transaction
+    end
+    it "will only consider installmets of memberships matching filter" do
+    	@membership_stats.installments.first.sum.should be_nil
+    end
+  end
+
   describe "with installments that have transactions" do
   	before do
   		@business = FactoryGirl.create(:school)
@@ -22,7 +37,7 @@ describe MembershipStats do
 
     it "should return installments" do
     	@membership_stats.installments.first.sum.should_not be_nil
-	end
+    end
   end
 
   describe "with installments that dont have transactions" do
@@ -35,6 +50,6 @@ describe MembershipStats do
 
     it "should not return installments" do
     	@membership_stats.installments.first.sum.should be_nil
-	end
+    end
   end
 end
