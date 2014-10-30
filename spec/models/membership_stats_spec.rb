a907939f70bbdc694bebe78e60ac9aff92f290ee
Luis Perichon <luisperichon@gmail.com>
2014-10-30 02:41:01 -0300

    fix spec

--- spec/models/membership_stats_spec.rb
+++ spec/models/membership_stats_spec.rb
@@ -28,7 +28,7 @@ describe MembershipStats do
     end
 
     it "sums paid and unpaid installments" do
-      stats.all_installments.first['sum'].should == (@paid_inst.value+@unpaid_inst.value)
+      expect(stats.all_installments.first['sum']).to == (@paid_inst.value+@unpaid_inst.value)
     end
   end
 
