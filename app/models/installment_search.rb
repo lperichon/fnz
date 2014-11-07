eecc96166c51efef43b2b7e66c6d601923fc1342
Luis Perichon <luisperichon@gmail.com>
2014-11-07 18:48:16 -0300

    installment search

---
+++ app/models/installment_search.rb
@@ -0,0 +1,28 @@
+class InstallmentSearch
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :due_after, :due_before, :status, :business_id, :agent_id
+
+  def initialize(attributes = {})
+    attributes.each do |name,value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
+
+  def results
+    scope = Installment.scoped
+    scope = scope.joins(:membership).where('memberships.business_id' => @business_id) unless @business_id.nil?
+    scope = scope.where("due_on >= ?", @due_after) unless @due_after.nil?
+    scope = scope.where("due_on <= ?", @due_before) unless @due_before.nil?
+    scope = scope.where(:status => @status) unless @status.nil?
+    scope = scope.where(:agent_id => @agent_id) unless @agent_id.nil?
+
+    scope
+  end
+end
---
+++ spec/models/installment_search_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe InstallmentSearch do
+  describe "persisted?" do
+    let(:ms){InstallmentSearch.new}
+    it "returns false" do
+      expect(ms).not_to be_persisted
+    end
+  end
+
+  context "with status" do
+    let(:status){:overdue}
+    let(:is_att){{status: status}}
+    let(:is){InstallmentSearch.new(is_att)}
+
+    let(:my_inst){FactoryGirl.create(:installment, status: status)}
+    let(:other_inst){FactoryGirl.create(:installment)}
+
+    it "includes installment in given status" do
+      expect(is.results).to include my_inst
+    end
+    it "excludes installments in other status" do
+      expect(is.results).not_to include other_inst
+    end
+  end
+
+  context "with due_after: 2014-1-1" do
+    let(:is_attributes){{due_after: Date.civil(2014,1,1)}}
+    let(:is){InstallmentSearch.new(is_attributes)}
+
+    describe "#results" do
+      let!(:yes){FactoryGirl.create(:installment,
+                                    due_on: Date.civil(2014,2,1))}
+      let!(:no){FactoryGirl.create(:installment,
+                                   due_on: Date.civil(2013,2,1))}
+      it "includes installment due on 2014-2-1" do
+        expect(is.results).to include yes
+      end
+      it "doesnt include installment due on 2013-2-1" do
+        expect(is.results).not_to include no
+      end
+    end
+  end
+end
