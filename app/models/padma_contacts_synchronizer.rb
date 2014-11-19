940066dfcaecefb3635549af602e943cb7243174
Luis Perichon <luisperichon@gmail.com>
2014-11-19 12:00:36 -0300

    fix bug, syncrhonizer was loading global status and teacher instead of local

--- app/models/padma_contacts_synchronizer.rb
+++ app/models/padma_contacts_synchronizer.rb
@@ -9,11 +9,11 @@ class PadmaContactsSynchronizer
     contacts = @business.contacts
     if contacts.empty? || @business.synchronized_at.nil?
       # Get all students
-      padma_contacts = PadmaContact.search(select: [:first_name, :last_name, :status, :global_teacher_username], :where => {:updated_at =>  @business.synchronized_at, :status => "student"}, per_page: 9999, username: @business.owner.username, account_name: @business.padma_id)
+      padma_contacts = PadmaContact.search(select: [:first_name, :last_name, :local_status, :local_teacher], :where => {:updated_at =>  @business.synchronized_at, :status => "student"}, per_page: 9999, username: @business.owner.username, account_name: @business.padma_id)
     else
       # Get all contacts updated after last sync
       padma_contact_ids = contacts.map(&:padma_id).uniq
-      padma_contacts = PadmaContact.search(select: [:first_name, :last_name, :status, :global_teacher_username], ids: padma_contact_ids, :where => {:updated_at =>  @business.synchronized_at}, per_page: padma_contact_ids.size, username: @business.owner.username, account_name: @business.padma_id)
+      padma_contacts = PadmaContact.search(select: [:first_name, :last_name, :local_status, :local_teacher], ids: padma_contact_ids, :where => {:updated_at =>  @business.synchronized_at}, per_page: padma_contact_ids.size, username: @business.owner.username, account_name: @business.padma_id)
     end
 
     padma_contacts.each do |padma_contact|
@@ -22,14 +22,14 @@ class PadmaContactsSynchronizer
           # If contact existed already update it
           contact.update_attributes(
   	        :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
-  	        :padma_status => padma_contact.status,
-  	        :padma_teacher => padma_contact.global_teacher_username)
+  	        :padma_status => padma_contact.local_status,
+  	        :padma_teacher => padma_contact.local_teacher)
         else
           # Otherwise create it
           @business.contacts.create(
             :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
-            :padma_status => padma_contact.status,
-            :padma_teacher => padma_contact.global_teacher_username,
+            :padma_status => padma_contact.local_status,
+            :padma_teacher => padma_contact.local_teacher,
             :padma_id => padma_contact.id)
         end
     end
