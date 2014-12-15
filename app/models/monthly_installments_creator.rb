cb338b1ccbd1245f0b62b4f6fee219170b8d5c5a
Luis Perichon <luisperichon@gmail.com>
2014-12-15 08:59:52 -0300

    update existint installment's agent monthly

--- app/models/monthly_installments_creator.rb
+++ app/models/monthly_installments_creator.rb
@@ -8,15 +8,21 @@ class MonthlyInstallmentsCreator
   def run
     business.contacts.students.each do |student|
       current_membership = student.current_membership
-      unless student.installment_for(Date.today).present? || current_membership.blank? || current_membership.overdue?
-        
+      installment = student.installment_for(Date.today)
+      
+      if installment.present?
+        # Update agent on already existing installments
         agent = business.agents.enabled.where(:padma_id => student.padma_teacher).first
-
+        if agent
+          installment.agent = agent
+          installment.save
+        end
+      else current_membership.present? and not current_membership.overdue?
+        # create installment
+        agent = business.agents.enabled.where(:padma_id => student.padma_teacher).first
         installment = current_membership.installments.new(:due_on => Date.new(Date.today.year,Date.today.month,current_membership.monthly_due_day),
                                                           :value => current_membership.value)
-        
         installment.agent = agent if agent
-
         installment.save
       end
     end
