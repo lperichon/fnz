3a7ab69228449b9fe5801ae0f46fcc3306969a10
Luis Perichon <luisperichon@gmail.com>
2015-02-26 08:26:52 -0300

    [#88178884] show delete button next to payment input field

--- app/inputs/payment_status_input.rb
+++ app/inputs/payment_status_input.rb
@@ -2,6 +2,11 @@ class PaymentStatusInput < SimpleForm::Inputs::Base
   def input
     out = '' # the output string we're going to build
     value = object.status
-    (out << template.content_tag(:span, value, :class => "label label-#{value}")).html_safe
+    business = options[:business]
+    out << template.content_tag(:span, value, :class => "label label-#{value}")
+    if !object.new_record? && !business.transactions_enabled? && object.complete?
+    	 out << template.link_to(template.content_tag(:i, "", :class => "icon-trash"), template.business_transaction_path(business, object.transactions.first), :confirm => template.t('actions.confirm_delete'), :method => 'delete')
+    end
+    out.html_safe
   end
 end
