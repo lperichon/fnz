1b7a64370878cc124154623b9923af82a9dc0a91
Dwayne Macgowan <dwaynemac@gmail.com>
2016-08-24 19:15:30 -0300

    refactor to DRY and reuse get_by_padma_id

--- lib/belongs_to_padma_contact.rb
+++ lib/belongs_to_padma_contact.rb
@@ -6,15 +6,7 @@ module BelongsToPadmaContact #:nodoc:
 
     def padma_contact_id= padma_contact_id
     	return if padma_contact_id.blank?
-	    unless c = Contact.where(padma_id: padma_contact_id, business_id: self.business_id).first
-	      padma_contact = PadmaContact.find(padma_contact_id, select: [:first_name, :last_name, :status, :global_teacher_username])
-	      c = Contact.create(padma_id: padma_contact_id,
-	                         business_id: self.business_id,
-	                         name: "#{padma_contact.first_name} #{padma_contact.last_name}",
-	                         padma_status: padma_contact.status,
-	                         padma_teacher: padma_contact.global_teacher_username)
-	    end
-
+      c = Contact.where(business_id: self.business_id).get_by_padma_id(padma_contact_id)
 	    self.contact = c
 	end
 
