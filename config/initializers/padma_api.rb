b0dcd0c5ae7a70d628d7bf44ac8f76e334132f8a
Dwayne Macgowan <dwaynemac@gmail.com>
2016-09-30 18:35:53 +0000

    consider cloud9 dev env

--- config/initializers/padma_api.rb
+++ config/initializers/padma_api.rb
@@ -12,13 +12,22 @@ end
 
 module Accounts
   API_KEY = ENV['accounts_key'] || CONFIG['accounts_key']
+  if ENV['C9_USER']
+    HOST = "padma-accounts-#{ENV['C9_USER']}.c9users.io"
+  end
 end
 
 module Contacts
   API_KEY = ENV['contacts_key'] || CONFIG['contacts_key']
+  if ENV['C9_USER']
+    HOST = "padma-contacts-#{ENV['C9_USER']}.c9users.io"
+  end
 end
 
 module Messaging
   API_KEY = ENV['messaging_key'] || CONFIG['messaging_key']
   API_SECRET = ENV['messaging_secret'] || CONFIG['messaging_secret']
+  if ENV['C9_USER']
+    HOST = "padma-messaging-#{ENV['C9_USER']}.c9users.io"
+  end
 end
