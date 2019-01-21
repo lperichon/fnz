f183a081f0c93e023ab3e302698b7ede571a9435
Dwayne Macgowan <dwaynemac@gmail.com>
2019-01-21 17:59:47 -0300

    linking transaction to installment sets agent and contact

--- app/models/installment_transaction.rb
+++ app/models/installment_transaction.rb
@@ -9,7 +9,18 @@ class InstallmentTransaction < ActiveRecord::Base
   after_save :update_installment
   after_destroy :update_installment
 
+  after_save :set_transaction_contact_and_agent
+
   def update_installment
     installment.update_balance_and_status
+  end
+
+  def set_transaction_contact_and_agent
+    if installment
+      transaction.update_attributes(
+        contact_id: installment.try(:membership).try(:contact_id),
+        agent_id: installment.agent_id
+      )
+    end
   end
 end
