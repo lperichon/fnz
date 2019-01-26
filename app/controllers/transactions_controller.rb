2b062c15f047d7b91b74bf6b8ff45ddcb85a9ce7
Dwayne Macgowan <dwaynemac@gmail.com>
2019-01-26 15:40:51 -0300

    avoid calling DB, even cached

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -18,7 +18,7 @@ class TransactionsController < UserApplicationController
                                 ((state.eq 'pending') & (transaction_at.lt start_date)) |
                                 ((state.eq 'reconciled') & (reconciled_at.gteq start_date) & (reconciled_at.lteq end_date))}
     end
-    @transactions = @context.includes(:agent, :contact, :tags, :source)
+    @transactions = @context.includes(:agent, :contact, :tags, :source, :business)
 
     respond_to do |format|
       format.html do
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -4,7 +4,19 @@ module TransactionsHelper
     best_in_place transaction,
                   :tag_id,
                   as: :select,
-                  collection: @business.tags.order(:name).map{|t| [t.id, t.name]},
+                  collection: tag_options_for_select,
                   url: business_transaction_path(@business, transaction)
+  end
+
+  def tag_options_for_select
+    @tag_options_for_select ||= @business.tags.order(:name).map{|t| [t.id, t.name]}
+  end
+
+  def agent_options_for_select
+    @agent_options_for_select ||= [["",""]]+@business.agents.enabled.map { |i| [i.id, i.name] }
+  end
+  
+  def contact_options_for_select
+    @contact_options_for_select ||= [["",""]]+@business.contacts.order(:name).map { |i| [i.id, i.name] }
   end
 end
