69a7bd245af197bef1b5ccda367b70b07abaadbb
Dwayne Macgowan <dwaynemac@gmail.com>
2019-03-21 11:37:38 -0300

    spec for api/v0/currrent_membership JSON

--- spec/controllers/api/v0/current_memberships_controller_spec.rb
+++ spec/controllers/api/v0/current_memberships_controller_spec.rb
@@ -1,16 +1,31 @@
 require 'spec_helper'
 
 describe Api::V0::CurrentMembershipsController, type: :controller do
-  let(:business){ FactoryGirl.create(:business) }
+  let(:business){ FactoryGirl.create(:school, padma_id: 'test') }
   let(:contacts){ (1..3).map{|i| FactoryGirl.create(:contact, business_id: business.id, padma_id: "padma-#{i}") } }
+  let!(:membership){ FactoryGirl.create(:membership, business: business, contact: contacts[0]) }
   describe "GET index" do
     before do
       get :index, app_key: ENV["app_key"],
-          business_id: business,
+          business_id: business.padma_id,
           padma_contact_ids: contacts.map(&:padma_id)
     end
     it "assigns memberships" do
       expect(assigns(:memberships)).not_to be_nil
+    end
+    it "assigns json" do
+      membership.reload
+      expect(assigns(:json).to_json).to eq [
+        {
+          value: membership.value,
+          begins_on: membership.begins_on,
+          ends_on: membership.ends_on,
+          payment_type: membership.payment_type.name,
+          padma_contact_id: membership.contact.padma_id,
+          name: membership.name,
+          installments: []
+        }
+      ].to_json
     end
   end
 end
--- spec/factories/memberships.rb
+++ spec/factories/memberships.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     contact {FactoryGirl.create(:contact, :business => self.business)}
     begins_on {Date.today.beginning_of_month}
     ends_on   {11.months.from_now.end_of_month}
+    payment_type { FactoryGirl.create(:payment_type) }
     monthly_due_day 10
     value 100
   end
