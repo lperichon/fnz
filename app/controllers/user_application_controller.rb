95ebef7c8707e448fed4c74bd2af088ec45373c2
Dwayne Macgowan <dwaynemac@gmail.com>
2019-10-28 12:22:28 -0300

    login con apikey para poder exportar csv a drive

--- app/controllers/user_application_controller.rb
+++ app/controllers/user_application_controller.rb
@@ -1,6 +1,7 @@
 class UserApplicationController < ApplicationController
   include SsoSessionsHelper
 
+  before_filter :secret_key_login
   before_filter :get_sso_session
   before_filter :authenticate_user!
   before_filter :set_current_user
@@ -21,5 +22,23 @@ class UserApplicationController < ApplicationController
 
     # If locale is sent by params override all
     I18n.locale = params[:locale] if params[:locale]
+  end
+
+  def secret_key_login
+    unless signed_in?
+      unless params[:secret_key_login].blank?
+        key = params[:secret_key_login]
+        apikey = Rails.cache.fetch("apikey#{key}", expires_in: 10.minutes) do
+          ApiKey.find key
+        end
+        if apikey.access == 'login_key'
+          user = nil
+          if apikey.username
+            user = User.find_or_create_by_drc_uid(apikey.username)
+          end
+          sign_in(user)
+        end
+      end
+    end
   end
 end
