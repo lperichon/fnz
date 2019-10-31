644356d4396af52cc382a4fb832a66ab0932593d
Dwayne Macgowan <dwaynemac@gmail.com>
2019-10-31 17:40:00 -0300

    avoid double submit on balance check

--- app/assets/javascripts/balance_checks/balance_modal_controller.js
+++ app/assets/javascripts/balance_checks/balance_modal_controller.js
@@ -7,8 +7,8 @@
     initialize(){ }
 
     connect(){
-      console.log("connected");
       this.expectedTarget.value = this.data.get("expectedBalance");
+      this.avoidDoubleSubmission();
     }
 
     removeModal(){
@@ -17,6 +17,14 @@
 
     updateDifference(){
       this.differenceTarget.value = this.balanceTarget.value - this.expectedTarget.value
+    }
+
+    avoidDoubleSubmission(){
+      console.log("bind avoidance");
+      $("#new_balance_check").submit(()=>{
+        console.log("disable");
+        $("#submitBalanceCheck").attr("disabled","disabled");
+      });
     }
 
 
