78471d174c507f446e0abb49ae2e7a644f27cc94
Dwayne Macgowan <dwaynemac@gmail.com>
2019-11-04 17:22:12 -0300

    allow calculations for amount

---
+++ app/assets/javascripts/calculator_controller.js
@@ -0,0 +1,21 @@
+(()=>{
+  stimulusApplication.register("calculator", class extends Stimulus.Controller {
+    static get targets(){
+      return ["formula","result"];
+    }
+
+    calculateResult(){
+      var result = null;
+      try {
+        result = math.evaluate(this.formulaTarget.value);
+      } catch (err) {
+        // do nothing
+      }
+      if(result){
+        this.resultTargets.forEach((i) => {
+          i.value = result;
+        });
+      }
+    }
+  });
+})();
