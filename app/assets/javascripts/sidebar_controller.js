082b1e3836231dc979e1f3477c853443383aece8
Dwayne Macgowan <dwaynemac@gmail.com>
2019-11-09 16:54:30 -0300

    toggle accounts sidebar

---
+++ app/assets/javascripts/sidebar_controller.js
@@ -0,0 +1,49 @@
+(()=>{
+  stimulusApplication.register("sidebar", class extends Stimulus.Controller {
+
+    static get targets(){
+      return ["sidebar","body","toggler","togglerVerb"];
+    }
+
+    connect(){
+      // sideBar starts visible
+      this.togglerVerbTarget.innerHTML = this.togglerTarget.dataset.hideMsg;
+
+      if(this.readCookieState() == "hidden"){
+        this.hideSideBar();
+      }
+    }
+
+    toggleSideBar(){
+      if(this.sidebarTarget.style.display=="none"){
+        this.showSideBar();
+      } else {
+        this.hideSideBar();
+      }
+    }
+
+    hideSideBar(){
+      this.sidebarTarget.style.display = "none";
+      $(this.bodyTarget).toggleClass("span9").toggleClass("span12").toggleClass("stickLeft");
+      this.togglerVerbTarget.innerHTML = this.togglerTarget.dataset.showMsg;
+
+      document.cookie = "sidebar=hidden";
+    }
+
+    showSideBar(){
+      $(this.bodyTarget).toggleClass("span9").toggleClass("span12").toggleClass("stickLeft");
+      this.sidebarTarget.style.display="block";
+      this.togglerVerbTarget.innerHTML = this.togglerTarget.dataset.hideMsg;
+      document.cookie = "sidebar=visible";
+    }
+
+    readCookieState(){
+      var cookie = document.cookie.split(";").filter((c)=>{ return c.match("sidebar") })[0];
+      if(cookie){
+        cookie.indexOf("=");
+        return cookie.substring(cookie.indexOf("=")+1,cookie.length);
+      }
+    }
+
+  });
+})();
