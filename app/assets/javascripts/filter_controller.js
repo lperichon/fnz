861cc0f3976cfdcce55293a1f384cbc6113f9139
Dwayne Macgowan <dwaynemac@gmail.com>
2019-11-13 13:20:12 -0300

    filter allows to set column with xx:

--- app/assets/javascripts/filter_controller.js
+++ app/assets/javascripts/filter_controller.js
@@ -1,7 +1,7 @@
 (()=>{
   stimulusApplication.register("filter", class extends Stimulus.Controller {
     static get targets(){
-      return ["query","item"];
+      return ["query","item","header"];
     }
 
     connect(){
@@ -10,11 +10,50 @@
     filterItems(event){
       if(this.queryTarget.value == ""){
         this.itemTargets.forEach( el => { el.classList.remove("filter--noMatch") });
+      } else if(this.queryIsSmart()){
+        this.smartFilter();
       } else {
+        this.generalFilter();
+      }
+    }
+
+    generalFilter(){
+      this.itemTargets.forEach((el,i) => {
+        el.classList.toggle("filter--noMatch", !el.textContent.toLowerCase().match(this.queryTarget.value.toLowerCase()));
+      });
+    }
+
+    smartFilter(){
+      if(this.smartColumnIndex()){
         this.itemTargets.forEach((el,i) => {
-          el.classList.toggle("filter--noMatch", !el.textContent.toLowerCase().match(this.queryTarget.value.toLowerCase()));
+          var content = el.getElementsByTagName("td")[this.smartColumnIndex()].textContent;
+          el.classList.toggle("filter--noMatch", !content.toLowerCase().match(this.smartValue().toLowerCase()));
         });
       }
+    }
+
+    queryIsSmart(){
+      return this.queryTarget.value.match(":");
+    }
+
+    smartColumnIndex(){
+      var tds = this.headerTarget.getElementsByTagName("td");
+      var i = 0;
+      var columnIndex = null;
+      for(i = 0; i < tds.length; i++){
+        if(tds[i].textContent.toLowerCase().match(this.smartColumnName().toLowerCase())){
+          columnIndex = i;
+        }
+      }
+      return columnIndex;
+    }
+
+    smartColumnName(){
+      return this.queryTarget.value.substring(0,this.queryTarget.value.match(":").index);
+    }
+
+    smartValue(){
+      return this.queryTarget.value.substring(this.queryTarget.value.match(":").index+1);
     }
   });
 
