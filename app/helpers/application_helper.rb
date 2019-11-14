049e45d43cf9a42b7eddfc0798d5e5c2d4c7c774
Dwayne Macgowan <dwaynemac@gmail.com>
2019-11-14 19:07:15 -0300

    link to see list in crm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,12 @@ module ApplicationHelper
     (controller.controller_name == "memberships" && controller.action_name == 'index') ||
     (controller.controller_name == "installments" && controller.action_name == 'index')
   end
+
+  def link_to_list_in_crm(contacts,options={})
+    url = "#{Crm::URL}/contacts?"
+    contacts.each do |c|
+      url << "contact_search[ids][]=#{c.padma_id}&"
+    end
+    link_to content_tag("i",class: "icon-list"){}+" "+ _("ver en CRM"), url, options
+  end
 end
