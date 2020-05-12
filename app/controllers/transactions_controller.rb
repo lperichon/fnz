0510de00b5fa4532d7cb0e0277a216a881744652
Dwayne Macgowan <dwaynemac@gmail.com>
2020-05-12 18:12:58 -0300

    fix call to params

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -103,9 +103,9 @@ class TransactionsController < UserApplicationController
   # PUT /accounts/1.json
   def update
     @transaction = Transaction.find(params[:id])
-    if transaction_attributes_for_update[:transaction][:type].in?(%W(Transfer Debit Credit))
+    if transaction_attributes_for_update[:type].in?(%W(Transfer Debit Credit))
       # This will make validations of the new desired type to be run on update_attributes
-      @transaction = @transaction.becomes(transaction_attributes_for_update[:transaction][:type].constantize)
+      @transaction = @transaction.becomes(transaction_attributes_for_update[:type].constantize)
     end
 
     respond_to do |format|
