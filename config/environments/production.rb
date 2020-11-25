692780e0c4f0bb3d012204bc9b35ce0e71b40488
Alex Falke <afalkear@gmail.com>
2020-11-25 17:23:24 -0300

    comment out deprecated options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Fnz::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # config.assets.compress = true
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
