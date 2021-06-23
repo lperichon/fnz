86ba6df6ce7d869941d6839e7acc11cb25892b0d
Dwayne Macgowan <dwaynemac@gmail.com>
2021-06-23 17:11:29 -0300

    threadsafe no longer needed or supported
    http://tenderlovemaking.com/2012/06/18/removing-config-threadsafe.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,6 @@ Fnz::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.threadsafe!
   config.cache_classes = false
 
   # set eager load
