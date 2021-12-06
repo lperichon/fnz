21795f134012f4177216eb66a1eb0fad2d0393af
Dwayne Macgowan <dwaynemac@gmail.com>
2021-12-05 21:00:32 -0300

    catch sns updated_contact and created_contact

--- app/models/message_processor.rb
+++ app/models/message_processor.rb
@@ -4,6 +4,8 @@ class MessageProcessor
   def self.catch_message(key_name, data)
     if key_name == "sso_session_destroyed"
       set_sso_session_destroyed_flag(data[:username])
+    elsif key_name == "updated_contact" || key_name == "created_contact"
+      Delayed::Job.enqueue FetchCrmContactJob.new(id: data[:id])
     end
     return unless key_name == 'subscription_change'
 
