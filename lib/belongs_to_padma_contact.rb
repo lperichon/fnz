29814994f4b4a806681d43c68787f13dd6bdcef4
Dwayne Macgowan <dwaynemac@gmail.com>
2022-01-07 19:18:34 -0300

    codestyle - indentations

--- lib/belongs_to_padma_contact.rb
+++ lib/belongs_to_padma_contact.rb
@@ -5,13 +5,13 @@ module BelongsToPadmaContact #:nodoc:
     #attr_accessible :padma_contact_id
 
     def padma_contact_id= padma_contact_id
-    	return if padma_contact_id.blank?
+      return if padma_contact_id.blank?
       c = Contact.where(business_id: self.business_id).get_by_padma_id(padma_contact_id)
-	    self.contact = c
-	end
+      self.contact = c
+    end
 
-	def padma_contact_id
-	    self.contact.try(:padma_id)
-	end
+    def padma_contact_id
+      self.contact.try(:padma_id)
+    end
   end
 end
