4a956dadd80b430cb328677d2b8864846a7f82df
Dwayne Macgowan <dwaynemac@gmail.com>
2022-02-05 20:47:30 -0300

    blockear transactions con report anterior a una fecha

--- db/migrate/20220205231435_block_transactions_before.rb
+++ db/migrate/20220205231435_block_transactions_before.rb
@@ -1,5 +1,6 @@
 class BlockTransactionsBefore < ActiveRecord::Migration
   def change
     add_column :businesses, :block_transactions_before, :datetime
+    Business.update_all block_transactions_before: 1.year.ago
   end
 end
