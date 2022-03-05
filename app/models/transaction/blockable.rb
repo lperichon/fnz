83c0d32d651fa75c6299d8450dc55ec76b095063
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-05 10:36:46 -0300

    keep block report_at exclusive

--- app/models/transaction/blockable.rb
+++ app/models/transaction/blockable.rb
@@ -18,16 +18,6 @@ module Transaction::Blockable
             errors.add(:report_at, t("transactions.you_blocked_all_before", date: business.block_transactions_before))
           end
         end
-        if transaction_at_changed?
-          if transaction_at < business.block_transactions_before
-            errors.add(:transaction_at, t("transactions.you_blocked_all_before", date: business.block_transactions_before))
-          end
-        end
-        if reconciled_at_changed?
-          if reconciled_at < business.block_transactions_before
-            errors.add(:reconciled_at, t("transactions.you_blocked_all_before", date: business.block_transactions_before))
-          end
-        end
       end
     end
 
