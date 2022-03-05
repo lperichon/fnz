8ea1ad56afaea3238b4e5a4248661dbec802d1b1
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-05 10:24:53 -0300

    block sepcs

---
+++ spec/models/transaction/may_be_blocked_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Transaction::MayBeBlocked' do
+  let(:block_since) { Date.civil(2020,1,1) }
+  let(:business) { FactoryBot.create(:business, block_transactions_before: block_since) }
+
+  describe "blocked?" do
+
+    it "true before block date" do
+      t = FactoryBot.build(:transaction, business: business, report_at: block_since-1.day)
+      expect(t).to be_blocked
+    end
+
+    it "false after block date" do
+      t = FactoryBot.build(:transaction, business: business, report_at: block_since+1.day)
+      expect(t).not_to be_blocked
+    end
+
+  end
+end
