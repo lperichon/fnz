4b01d6d25e14f181cc56f5806ff8fb03e9d07226
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-06 12:28:19 -0300

    [blockable] change_allowed? public

--- app/models/transaction/blockable.rb
+++ app/models/transaction/blockable.rb
@@ -12,7 +12,13 @@ module Transaction::Blockable
     end
 
     def wont_violate_block
-      if business.block_transactions_before
+      if blocked?
+        unless changed.all? { |attr| change_allowed?(attr) }
+          errors.add(:report_at,
+            I18n.t("transactions.you_blocked_all_before",
+              date: business.block_transactions_before))
+        end
+      elsif business.block_transactions_before
         if report_at_changed?
           if report_at < business.block_transactions_before
             errors.add(:report_at,
--- spec/models/transaction/blockable_spec.rb
+++ spec/models/transaction/blockable_spec.rb
@@ -18,18 +18,35 @@ describe 'Transaction::Blockable' do
 
   end
 
-  it "cant add transaction before block" do
-    t = FactoryBot.build(:transaction, business: business, report_at: block_since-1.day)
-    expect(t).not_to be_valid
-  end
-  it "cant update transaction reporting it before block" do
-    t = FactoryBot.create(:transaction, business: business, report_at: block_since+1.day)
-    expect(t).to be_valid
+  describe "if not blocked" do
+    it "cant add transaction before block" do
+      t = FactoryBot.build(:transaction, business: business, report_at: block_since-1.day)
+      expect(t).not_to be_valid
+    end
+    it "cant update transaction reporting it before block" do
+      t = FactoryBot.create(:transaction, business: business, report_at: block_since+1.day)
+      expect(t).to be_valid
 
-    expect( t.update_attributes(report_at: block_since-1.day) ).to be_falsey
+      expect( t.update_attributes(report_at: block_since-1.day) ).to be_falsey
 
-    t.report_at = block_since-1.day
-    expect(t).not_to be_valid
+      t.report_at = block_since-1.day
+      expect(t).not_to be_valid
+    end
   end
+  describe "if blocked" do
+    let(:t) { t = FactoryBot.build(:transaction, business: business, report_at: block_since - 1.day, state: "pending" ); t.save(validate: false); t }
+    it "CAN change state and reconciled_at of a blocked transaction" do
+      expect(t.update_attributes(state: "reconciled", reconciled_at: block_since+1.day) ).to be_truthy
+    end
+    it "canT change non allowed attributes" do
+      expect(t.update_attributes(amount_cents: 1000) ).to be_falsey
+      expect(t.update_attributes(state: "pending", amount_cents: 1000) ).to be_falsey
+    end
+  end
+
+
+
+
+
 
 end
