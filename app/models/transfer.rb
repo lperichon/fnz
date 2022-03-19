e23a5212e2048c0ba472a10e7d6366f8932314cb
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-19 10:07:13 -0300

    style

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,8 +1,8 @@
 class Transfer < Transaction
 
   # explicity check for type=="Transfer" for cases when type is changed on update_attributes
-  validates :target, :presence => true, if: ->{ self.type == "Transfer" }
-  validates :conversion_rate, :presence => true, :numericality => {:greater_than => 0}, if: ->{ self.type == "Transfer" }
+  validates :target, presence: true, if: ->{ self.type == "Transfer" }
+  validates :conversion_rate, presence: true, numericality: {greater_than: 0}, if: ->{ self.type == "Transfer" }
 
   def sign account
     sign = 0
