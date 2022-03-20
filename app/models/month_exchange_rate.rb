d2a1c84acd14d6d4cac03fa7e8749d9cd36a8064
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-20 10:43:05 -0300

    make method public

--- app/models/month_exchange_rate.rb
+++ app/models/month_exchange_rate.rb
@@ -29,6 +29,33 @@ class MonthExchangeRate < ActiveRecord::Base
     end
   end
 
+  def update_calculations
+    if conversion_rate_changed?
+      business.accounts.where(currency: [from_currency_id, to_currency_id]).each do |account|
+        try_with_transfers = true
+        # Recalcular reportes. 1 x tag
+        Transaction.to_report_on_month(ref_date)
+                   .where("source_id = :account_id OR target_id = :account_id", account_id: account.id)
+                   .select("DISTINCT ON (admpart_tag_id) * ") # esto hace que me devuelva 1 x tag. @see https://stackoverflow.com/questions/3800551/select-first-row-in-each-group-by-group , TODO confirmar
+                   .each do |transaction|
+          transaction.delay.update_balances
+          try_with_transfers = false
+        end
+
+        if try_with_transfers
+          # Todavía No se disparó actualización en esta account
+          # Recalcular balances de cuentas. Solo transfers, 1 x account suficiente.
+          # El conversion rate solo afecta el balance de la target, no de la source
+          if (transfer = Transfer.where("transaction_at >= ? AND transaction_at <= ?", ref_date.beginning_of_month, ref_date.end_of_month)
+                                 .where(target_id: account.id)
+                                 .first)
+            transfer.delay.update_balances
+          end
+        end
+      end
+    end
+  end
+
   private
 
   def different_currencies
@@ -64,32 +91,6 @@ class MonthExchangeRate < ActiveRecord::Base
     end
     if to_currency_id.downcase != to_currency_id
       self.to_currency_id = to_currency_id.downcase
-    end
-  end
-
-  def update_calculations
-    if conversion_rate_changed?
-      business.accounts.where(currency: [from_currency_id, to_currency_id]).each do |account|
-        try_with_transfers = true
-        # Recalcular reportes. 1 x tag
-        Transaction.to_report_on_month(ref_date)
-          .where("source_id == :account_id OR target_id = :account_id", account_id: account.id)
-          .select("DISTINCT ON (admpart_tag_id) * ") # esto hace que me devuelva 1 x tag. @see https://stackoverflow.com/questions/3800551/select-first-row-in-each-group-by-group , TODO confirmar
-          .each do |transaction|
-          transaction.delay.update_balances
-          try_with_transfers = false
-        end
-
-        if try_with_transfers
-          # Todavía No se disparó actualización en esta account
-          # Recalcular balances de cuentas. Solo transfers, 1 x account suficiente.
-          # El conversion rate solo afecta el balance de la target, no de la source
-          Transfer.where("transaction_at >= ? AND transaction_at <= ?", ref_date.beginning_of_month, ref_date.end_of_month)
-                  .where(target_id: account.id)
-                  .first
-                  .delay.update_balances
-        end
-      end
     end
   end
 
