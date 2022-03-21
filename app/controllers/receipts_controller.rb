1015e96023af9a7fac2c6c4d7e4b4ac4c27a13b1
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-21 10:22:57 -0300

    [receipts] locale

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -2,8 +2,28 @@ class ReceiptsController < ApplicationController
 
   layout "receipts"
 
+  before_action :set_locale
+
   def show
     @receipt = Receipt.w_secret(params[:s]).find(params[:id])
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = if current_user
+      currency_user.locale
+    elsif params[:locale]
+      params[:locale]
+    else
+      extract_browser_locale(request.env['HTTP_ACCEPT_LANGUAGE'])
+    end
+  end
+
+  def extract_browser_locale(http_accept_language)
+    http_accept_language.to_s.scan(/[a-z]{2}(?:-[A-Z]{2})?/).detect do |candidate|
+      I18n.available_locales.include?(candidate.to_sym)
+    end
   end
 
 end
