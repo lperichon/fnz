2540e4b430abf1830fb230b03e91f542725150f3
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-21 10:43:30 -0300

    [receipts] share to whatsapp link

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -7,4 +7,9 @@ module ReceiptsHelper
       receipt_url(id: receipt.id, s: receipt.url_secret)
     end
   end
+
+  def whatsapp_url(receipt)
+    text = %[#{t(".receipt")}##{@receipt.id}: #{number_to_currency(@receipt.amount, unit: @receipt.currency.symbol)}\n#{public_receipt_url(receipt)}]
+    "https://api.whatsapp.com/send?text=#{URI.encode(text)}"
+  end
 end
