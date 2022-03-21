a0f04851adc40802354cc3d835b274743611a0b7
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-21 09:15:52 -0300

    style

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,18 +10,18 @@ class Ability
     can :manage, Business, id: user.owned_businesses.pluck(:id)
     can :manage, RecurrentTransaction, business: {transactions_enabled: true, id: user.business_ids}
 
-    can :manage, Admpart, :business => {:transactions_enabled => true}
-    can :manage, Transaction, :business => {:transactions_enabled => true}
+    can :manage, Admpart, business: {transactions_enabled: true}
+    can :manage, Transaction, business: {transactions_enabled: true}
     cannot [:create, :update, :split, :destroy], Transaction do |t|
       t.blocked? && t.state != "pending"
     end
-    can :manage, Account, :business => {:transactions_enabled => true}
-    can :manage, BalanceCheck
+    can :manage, Account, business: {transactions_enabled: true}
+    can = can :manage, BalanceCheck
 
-    can :manage, Tag, :business => {:transactions_enabled => true}
+    can :manage, Tag, business: {transactions_enabled: true}
     cannot :destroy, Tag, system_name: Tag::VALID_SYSTEM_NAMES
     
-    can :manage, Import, :business => {:transactions_enabled => true}
-    can :manage, User, :business => {:share_enabled => true}
+    can :manage, Import, business: {transactions_enabled: true}
+    can :manage, User, business: {share_enabled: true}
   end
 end
