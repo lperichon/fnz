36d50425d48c4bb5c137ed71eddcf677c83f2d80
Dwayne Macgowan <dwaynemac@gmail.com>
2022-03-21 09:45:12 -0300

    receipt default currency

--- app/models/receipt/has_currency.rb
+++ app/models/receipt/has_currency.rb
@@ -3,10 +3,20 @@ module Receipt::HasCurrency
 
   included do
 
+    before_validation :set_default_currency
+
     validates :currency_id, presence: true
 
     def currency
       @currency ||= Currency.find(currency_id)
+    end
+
+    private
+
+    def set_default_currency
+      if self.currency_id.blank?
+        self.currency_id = business.currency_code
+      end
     end
 
   end
