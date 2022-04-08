fc6c27a24412f9a0ce7dd517b15f2bb6acb5da6a
Dwayne Macgowan <dwaynemac@gmail.com>
2022-04-08 12:00:34 -0300

    avoid nil

--- db/migrate/20220408145358_receipt_hm_transactions.rb
+++ db/migrate/20220408145358_receipt_hm_transactions.rb
@@ -3,7 +3,9 @@ class ReceiptHmTransactions < ActiveRecord::Migration
     add_column :transactions, :receipt_id, :integer
     add_index :transactions, :receipt_id
     Receipt.all.each do |r|
-      r.trans.update_columns(receipt_id: r.id)
+      if r.trans
+        r.trans.update_columns(receipt_id: r.id)
+      end
     end
     remove_column :receipts, :transaction_id
   end
