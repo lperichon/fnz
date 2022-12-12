36dced0c85b67b5702b7f88454c389bc358e2cd6
Dwayne Macgowan <dwaynemac@gmail.com>
2022-12-12 15:20:52 -0300

    [railway] use redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
   if ENV["MEMCACHIER_SERVERS"]
     config.cache_store = :dalli_store, ENV["MEMCACHIER_SERVERS"].split(','), { :username => ENV["MEMCACHIER_USERNAME"], :password => ENV["MEMCACHIER_PASSWORD"] }
+  elsif ENV["REDIS_URL"]
+    config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
   end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
