d2ad74ea39b9f1326ac88869276801d0194a22c3
Dwayne Macgowan <dwaynemac@gmail.com>
2022-12-12 16:03:32 -0300

    [railway] dont call on boot

--- config/clock.rb
+++ config/clock.rb
@@ -15,10 +15,10 @@ module Clockwork
     %x{ #{job} }
   end
 
-  every(1.hour, "rake sync_with_padma_accounts")
+  every(1.hour, "rake sync_with_padma_accounts", skip_first_run: true)
 
-  every(1.day, "rake create_monthly_installments", at: "01:00")
-  every(1.day, "rake broadcast_end_of_memberships", at: "02:00")
-  every(1.day, "rake create_recurrent_transactions", at: "03:00")
-  every(1.day, "rake update_current_memberships", at: "04:00")
+  every(1.day, "rake create_monthly_installments", at: "01:00", skip_first_run: true)
+  every(1.day, "rake broadcast_end_of_memberships", at: "02:00", skip_first_run: true)
+  every(1.day, "rake create_recurrent_transactions", at: "03:00", skip_first_run: true)
+  every(1.day, "rake update_current_memberships", at: "04:00", skip_first_run: true)
 end
